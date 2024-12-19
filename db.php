2733258e450962011147fc778e75b5b57552a4ba
lbiggers <lbiggers@mtu.edu>
2024-12-19 17:17:57 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db = 'main';
-$user = 'deqd';
-$pass = '';
+$user = 'server';
+$pass = 'P@ssow0rd';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
