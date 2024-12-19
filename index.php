53ae7af50f637ef58a68fb5cd29869ed1b38263f
lbiggers <lbiggers@mtu.edu>
2024-12-19 18:16:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
+<?php
+require_once 'db.php';
+session_start();
+if(!isset($_SESSION['uname'])){
+	//header('LOCATION:login.php');
+	?>	
+		<form method='post'>
+			<label>Username:</label>
+			<input type='text' name='uname'>
+			<br>
+			<label>Password:</label>
+			<input type='password' name='passwd'>
+			<br>
+			<input type='submit' name='login' value='Login'>
+		</form>
+	<?php
+} else {
+	?>
+		<h1>success</h1>
+	<?php
+}
+if(isset($_POST['login'])){
+	$_SESSION['uname'] = $_POST['uname'];
+}
+?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Cloney Clicker</title>
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<h1>Hi mommy</h1>
-		<script src="script.js"></script>
-	</body>
+<head>
+	<title>Cloney Clicker</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<script src="script.js"></script>
+</body>
 </html>
