a48a1837806c22f96cc97a54343babbaa85c9fec
lbiggers <lbiggers@mtu.edu>
2024-12-19 12:15:03 -0500

    basic

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cloney Clicker</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<h1>
+			<?php echo'go php'; ?>
+		</h1>
+		<script src="script.js"></script>
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+let elem = document.createElement('h1');
+elem.innerText = 'js go';
+document.body.appendChild(elem);
